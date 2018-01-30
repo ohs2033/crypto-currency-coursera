3c6cd9172c5ef59d4fcb9c9f1f05c39c60a0a4c7
USER <user@AL01297799.local>
2018-01-30 18:32:26 +0900

    fix

--- TxHandler.java
+++ TxHandler.java
@@ -13,7 +13,6 @@ public class TxHandler {
      */
 	UTXOPool uxp;
     public TxHandler(UTXOPool utxoPool) {
-    		// 장부를 만들라고? 어떠헥 장부를 만들라는거지. UTXOPoo컨스트럭터를 이용해서 utxpool의 카피를 만들어야 한다.
     		this.uxp = new UTXOPool(utxoPool);
     }
 
@@ -21,32 +20,30 @@ public class TxHandler {
      * @return true if:
      * (1) all outputs claimed by {@code tx} are in the current UTXO pool,  :done
      * (2) the signatures on each input of {@code tx} are valid,   : done
-     * (3) no UTXO is claimed multiple times by {@code tx}, :이거하나만 하면됨!
+     * (3) no UTXO is claimed multiple times by {@code tx}, 
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
         UTXOPool uniqueUtxos = new UTXOPool();
-    		byte[] hash = tx.getHash();
-        ArrayList<Transaction.Input> inputs = new ArrayList<Transaction.Input>(tx.getInputs());
-        ArrayList<Transaction.Output> outputs = new ArrayList<Transaction.Output>(tx.getOutputs());
+        ArrayList<Transaction.Output> outputs = tx.getOutputs();
+        
         int ipSum = 0;
         int opSum = 0;
         
         for (Transaction.Output op : outputs) {
-        		int index = outputs.indexOf(op);
-        		UTXO tempUt = new UTXO(hash, index);
+        		if (op.value < 0) return false;
         		opSum += op.value;
         }
         
-        for (Transaction.Input ip : inputs) {
+        for (int i = 0; i < tx.numInputs(); i++) {
+
+        		Transaction.Input ip = tx.getInput(i);
         		UTXO ux = new UTXO(ip.prevTxHash, ip.outputIndex);
         		Transaction.Output tempOp = uxp.getTxOutput(ux);
         		ipSum += tempOp.value;
         		if (!uxp.contains(ux)) return false;
-        		//signature.
-        		
-        		if(!Crypto.verifySignature(tempOp.address, tx.getRawDataToSign(inputs.indexOf(ip)), ip.signature)) return false;
+        		if(!Crypto.verifySignature(tempOp.address, tx.getRawDataToSign(i), ip.signature)) return false;
         		if (uniqueUtxos.contains(ux)) return false;
                 uniqueUtxos.addUTXO(ux, tempOp);
         }
@@ -54,11 +51,7 @@ public class TxHandler {
         if(ipSum < opSum) return false;
         
     		return true;
-    		// 모든 아웃풋이 현재 UTXO pool에 있는지 체크하는 코드.
-    		// tx.outputs을 전부 돌면서?
-    		// 각 input의 signature가 valid한지 체크해야한다.
-    		// 어떤 UTX도 여러번 선언되면 안된다.
-    		//  input value가 out valu의 합이랑 각각 같아야 한다.
+
     }
 
     /**
@@ -88,9 +81,6 @@ public class TxHandler {
          Transaction[] validTxArray = new Transaction[validTxs.size()];
          return validTxs.toArray(validTxArray);
 
-    		// possibleTxs들을 인자로 받아서 뭘 한다는걸까?
-    		// 각 트랜잭션의 정확도를 체크해서, 상호-valid한 거래들로 수용된 ㅡ랜잭션을 만들어서, UTXO pool을 업데이트 하라는거지?
-        // IMPLEMENT THIS
     }
 
 }
